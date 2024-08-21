e7cec53ed9b9c334548f63e305a4670bf85c5cdb
Atilla-Koz <atillakoz@hotmail.com>
2024-08-21 22:33:35 +0300

    chore: Update landing page with new banner color and additional text

--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -4,13 +4,16 @@ export default function Home() {
   return (
     <section className="h-screen bg-custom-bg bg-cover bg-center font-uncial">
       <div className="h-screen flex flex-col items-center justify-center">
-        <h1 className="text-white">Vampire And Villagers</h1>
+        <h1 className="text-[red]/65 ">Vampire And Villagers</h1>
         <Link to="/role">
           {' '}
           <button className="p-[15px 40px 15px 40px] bg-white/50 ]">
             <h3 className="text-black/50">Start</h3>
           </button>
         </Link>
+        <h2 className="text-white/75 text-center ">
+          Enjoyable games, with love from Atilla Koz...{' '}
+        </h2>
       </div>
     </section>
   );
